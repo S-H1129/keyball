9254549a5a933a2d3e17ee1fd94e0a131db6ef97
S-H1129 <s.hiroaki.w0w@gmail.com>
2025-01-06 00:22:45 +0900

    indent

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -32,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_universal(
     SSNP_FRE ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_7    , KC_8    , KC_9    , KC_F9    , KC_F10   , KC_F11   ,
-    SSNP_VRT ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_4  , KC_5  , KC_6    , KC_BTN2  , KC_BTN3  , KC_F12   ,
-    SSNP_HOR ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_1  , KC_2  , KC_3  , KC_RGHT  , _______  , _______  ,
+    SSNP_VRT ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_4    , KC_5    , KC_6    , KC_BTN2  , KC_BTN3  , KC_F12   ,
+    SSNP_HOR ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_1    , KC_2    , KC_3    , KC_RGHT  , _______  , _______  ,
                   _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
 
@@ -53,32 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
-// Customize keycodes
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    default:
-      return true; // Process all other keycodes normally
-  }
-}
-
 layer_state_t layer_state_set_user(layer_state_t state) {
-      switch(get_highest_layer(remove_auto_mouse_layer(state, true))) {
-        case 2:
-            // Finish auto mouse layer when the highest layer is 2
-            state = remove_auto_mouse_layer(state, false);
-            break;
-        case 3:
-            // Auto enable scroll mode when the highest layer is 3
-            // remove_auto_mouse_target must be called to adjust state *before* setting enable
-            state = remove_auto_mouse_layer(state, false);
-            set_auto_mouse_enable(false);
-            keyball_set_scroll_mode(true);
-            break;
-        default:
-            set_auto_mouse_enable(true);
-            keyball_set_scroll_mode(false);
-            break;
-    }
+    // Auto enable scroll mode when the highest layer is 3
+    keyball_set_scroll_mode(get_highest_layer(state) == 3);
     return state;
 }
 
